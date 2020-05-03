40ab8eadfe512a96c669ed364a8edba4f73907cc
Weijie <tanweijie@gmail.com>
2020-05-04 07:47:26 +0800

    Initial commit

---
+++ src/main/java/org/weijie/playspringrest/PlaySpringRestApplication.java
@@ -0,0 +1,13 @@
+package org.weijie.playspringrest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlaySpringRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlaySpringRestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/weijie/playspringrest/PlaySpringRestApplicationTests.java
@@ -0,0 +1,13 @@
+package org.weijie.playspringrest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlaySpringRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
